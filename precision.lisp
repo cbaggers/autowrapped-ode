a96668b33eb7179ac944cab242f706d16fa1fc55
Baggers <chris.bagley@gmail.com>
2016-09-10 16:55:56 +0200

    crash on double precision lib

--- precision.lisp
+++ precision.lisp
@@ -4,9 +4,10 @@
 ;; Precision
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-
   ;; figure out if this library is single or double precision...
-  (defconstant +ode-precision+
-    (if (search "ODE_double_precision" (cffi:foreign-funcall "dGetConfiguration" :string))
-        :double
-        :single)))
+  (when (eq (search "ODE_double_precision" (cffi:foreign-funcall "dGetConfiguration" :string))
+            :double)
+    (error "The autowrapped-ode bindings currently only support the
+single precision floating point version of the ODE library.
+
+Sorry for the inconvenience")))
